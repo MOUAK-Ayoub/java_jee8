c18a6503f6a49f8f69441db7a330d005ea564da3
ayoub.mouak <ayoub.mouak.2015@gmail.com>
2020-02-17 10:56:59 +0100

    Tutorial Servlet+ JSP +JDBC

---
+++ src/com/chap03/HelloWorldController.java
@@ -0,0 +1,42 @@
+package com.chap03;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.annotation.*;
+import javax.faces.bean.*;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named(value = "helloWorldController")
+@RequestScoped
+public class HelloWorldController implements Serializable {
+    private String hello;
+
+    /**
+     * Creates a new instance of HelloWorldBean
+     */
+    public HelloWorldController() {
+        System.out.println("Instantiated ");
+
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Instantiated helloWorldController");
+        hello = "Hello World";
+    }
+
+    /**
+     * @return the hello
+     */
+    public String getHello() {
+        return hello;
+    }
+
+    /**
+     * @param hello the hello to set
+     */
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
+}
---
+++ src/com/chap03/JsfConfiguration.java
@@ -0,0 +1,7 @@
+package com.chap03;
+
+import javax.faces.annotation.*;
+
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+public class JsfConfiguration {
+}
